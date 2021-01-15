73707a322a144080984529bd950be3bd6640aa90
Stephen Foo Bar <stephenfoobar@gmail.com>
2021-01-15 17:03:23 -0500

    Removing commented code

--- src/eventsParser.js
+++ src/eventsParser.js
@@ -2,18 +2,6 @@ const fs = require('fs');
 var events = [];
 var errors = [];
 
-// function parse(parsingFunction, fileName, identifier) {
-//     try {
-//         events = parsingFunction(fileName, identifier);    
-//     } catch (error) {
-//         errors.push(error);
-//     }
-//     if (!events)
-//         errors.push("Unknown error occurred.");
-//     if (events.length == 0)
-//         errors.push("No events found.");
-// }
-
 function parse(parsingFunction) {
     try {
         events = parsingFunction();
